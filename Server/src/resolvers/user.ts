cf0a92a5756bc774471a75c0e05a5345a5736a98
MananDesai54 <manan5401desai@gmail.com>
2020-12-21 23:47:35 +0530

    CRUD with type=GraphQL and pgSQL done

--- Server/src/resolvers/user.ts
+++ Server/src/resolvers/user.ts
@@ -1,27 +1,63 @@
-import { Arg, Args, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
-import { MyContext, UserData } from "../config/types";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { MyContext } from "../config/types";
 import { User } from "../entities/User";
 
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
-  users(@Ctx() context: MyContext): Promise<User[]> {
-    return context.em.find(User, {});
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
   }
 
   @Query(() => User, { nullable: true })
-  user(
-    @Ctx() context: MyContext,
-    @Arg("id", () => Int) id: number
-  ): Promise<User | null> {
-    return context.em.findOne(User, { id });
+  user(@Ctx() { em }: MyContext, @Arg("id") id: number): Promise<User | null> {
+    return em.findOne(User, { id });
   }
 
   @Mutation(() => User)
-  registerUser(
-    @Ctx() context: MyContext,
-    @Arg("userData", () => User) userData: UserData
-  ) {
-    return "hh";
+  async registerUser(
+    @Ctx() { em }: MyContext,
+    @Arg("username") username: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const user = em.create(User, {
+      username,
+      password,
+      email,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async updateUser(
+    @Ctx() { em }: MyContext,
+    @Arg("id") id: number,
+    @Arg("email", () => String, { nullable: true }) email: string,
+    @Arg("username", () => String, { nullable: true }) username: string
+  ): Promise<User | null> {
+    const user = await em.findOne(User, { id });
+    if (!user) {
+      return null;
+    }
+    if (email) user.email = email;
+    if (username) user.username = username;
+
+    await em.persistAndFlush(user);
+    return user;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteUser(
+    @Ctx() { em }: MyContext,
+    @Arg("id") id: number
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(User, { id });
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 }

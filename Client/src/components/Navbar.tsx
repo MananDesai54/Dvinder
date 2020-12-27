e7351a358bec543dc57f0b183a6f45c1e26b3aac
MananDesai54 <manan5401desai@gmail.com>
2020-12-27 12:01:31 +0530

    come back

---
+++ Client/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { FC } from "react";
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = ({}) => {
+  return <Box bg="tomato">navbar</Box>;
+};
+
+export default Navbar;

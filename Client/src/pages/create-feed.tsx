0b3ecb2bf0ffaad94aa8495d44beef48af9453b4
MananDesai54 <manan5401desai@gmail.com>
2021-01-01 00:46:27 +0530

    with feeds

---
+++ Client/src/pages/create-feed.tsx
@@ -0,0 +1,38 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import React, { FC } from "react";
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
+import { useCreateFeedMutation } from "../generated/graphql";
+
+interface CreateFeedProps {}
+
+const CreateFeed: FC<CreateFeedProps> = ({}) => {
+  const [, createFeed] = useCreateFeedMutation();
+
+  return (
+    <Formik
+      // username included for matching types for handleAuthAndError
+      initialValues={{ title: "", imageUrl: "" }}
+      onSubmit={async (values, errors) => {
+        const response = await createFeed(values);
+      }}
+    >
+      {() => (
+        <Wrapper variant="small">
+          <Form>
+            <InputField name="title" type="text" label="Title" />
+            <Box mt={4}>
+              <InputField name="imageUrl" type="text" label="ImageUrl" />
+            </Box>
+            <Button mt={4} colorScheme="teal" type="submit">
+              Create Feed
+            </Button>
+          </Form>
+        </Wrapper>
+      )}
+    </Formik>
+  );
+};
+
+export default CreateFeed;
